9a5f4bad4a59f26a42196fa113a78fd0ad6403d5
Pengu-8 <123yeeheng.com@gmail.com>
2024-11-19 22:12:24 +1100

    try

--- portfolio/src/Main.js
+++ portfolio/src/Main.js
@@ -1,10 +1,55 @@
+import gitimg from "./assets/Github.png";
 
-function main() {
-    return(
+function Main() {
+    function PianoTiles () {
+
+    }
+    return (
         <>
+            <div>
+                <h1 id="title">Welcome to Yeeheng's Portfolio</h1>
+            </div>
+            <div id="keyboard">
+                <a href="https://github.com/Pengu-8" id="whitelink">
+                    <img src={gitimg} alt="Github" id="linkimg"/>
+                </a>
+                {/*<img src="assets/Github.png" alt="Github"/>*/}
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
+                <button className="BlackTiles"></button>
+                <button className="WhiteTiles"></button>
 
+            </div>
         </>
     )
 }
 
-export default main;
+export default Main;
